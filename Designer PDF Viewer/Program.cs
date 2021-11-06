6ea5af0a442028ccdd918f16140bba14192bb85d
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-11-06 19:48:32 +0500

    Add project files.

---
+++ Designer PDF Viewer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Designer_PDF_Viewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] h = Array.ConvertAll(Console.ReadLine().Split(' '), hTemp => Convert.ToInt32(hTemp));
+            string word = Console.ReadLine();
+
+            int result = designerPdfViewer(h, word);
+            Console.WriteLine(result);
+        }
+        static int designerPdfViewer(int[] h, string word)
+        {
+            List<int> lengths = new List<int>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                lengths.Add(h[word[i] - 97]);
+            }
+            int max = lengths.Max();
+
+            return word.Length * max;
+        }
+    }
+}
